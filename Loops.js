97295c7e4faaeeef06276fd1b176389586871a6a
davidshalom-git <davidshalom2609@gmail.com>
2025-09-09 06:22:02 +0530

    DSA

--- Loops.js
+++ Loops.js
@@ -167,6 +167,22 @@ function SecondLargest(arr){
 console.log("Second Largest " + SecondLargest(arrS))
 
 
+// Nested For Loop
+
+for(let i = 0;i<3;i++){
+    for(let j = 0;j<3;j++){
+        console.log("i= " + i + " j=" + j)
+    }
+}
+
+console.log("-----------------------------------------------------------------------------")
+
+for(let i = 0;i<3;i++){
+    for(let j = 0;j<i;j++){
+        console.log("i= " + i + " j=" + j)
+    }
+}
+
 
 
 
