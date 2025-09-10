3ceee02f8b590619435817aa637a1a8adaeda8c3
davidshalom-git <davidshalom2609@gmail.com>
2025-09-10 20:03:32 +0530

    DSA

--- Star.js
+++ Star.js
@@ -14,7 +14,7 @@
 // let n = 4;
 // for(let i = 0;i<n;i++){
 //     let row = ""
-//     for(let j = 0;j<n;j++){
+//     for(let j = 0; j<n; j++){
 //         row = row + "*"
 //     }
 //     console.log(row);
@@ -33,7 +33,7 @@
 let n = 4
 for(let i = 0;i<n;i++){
     let row = ""
-    for(let j = 0;j<i+1;j++){
+    for(let j = 0; j<i+1; j++){
         row = row + "*"
     }
     console.log(row);
