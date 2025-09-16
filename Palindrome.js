36a86257fab1a881755ed3bb879631b7675d3ede
davidshalom-git <davidshalom2609@gmail.com>
2025-09-16 21:11:40 +0530

    DSA

--- Palindrome.js
+++ Palindrome.js
@@ -1,6 +1,25 @@
 // Concept to be remember..
 
-// 1. Last digit => n%10
-// 2. remove last => n/10
+// 1. Last digit => n%10 => reminder
+// 2. remove last => n/10 => quotient
 
+let n = 121
+let ncopy = n
+function Palindrome(n,x){
+    let rev = 0;
+    while(n>0){
+       let rem = n%10;
+       rev = (10*rev) + rem;
+       n = Math.floor(n/10);
+    }
+    if(rev === x){
+        return true;
+    }
+    else
+    {
+        return false
+    }
+}
+
+console.log(Palindrome(n,ncopy))
 
