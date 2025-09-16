42a6f48ad481844dadb3d0b3e07059a2d82f9af6
davidshalom-git <davidshalom2609@gmail.com>
2025-09-16 20:10:20 +0530

    DSA

---
+++ Count_Digit.js
@@ -0,0 +1,20 @@
+
+
+function Count(){
+    let count = 0;
+
+    let n = 259;
+
+// divide the n/10 to count the number;
+
+    while(n>0){
+        n = Math.floor(n/10);
+        count++;
+        // Math.floor rounds down the value, if it contains point numbers, it will round the numbers;
+    }
+    return count;
+}
+
+console.log(Count());
+
+
