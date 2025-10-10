ba49a268f3a1db7801f53329c112ef156b09d711
davidshalom-git <davidshalom2609@gmail.com>
2025-10-10 12:00:41 +0530

    DSA

--- Array.js
+++ Array.js
@@ -48,4 +48,25 @@ function StringReverse(arr){
     }
 }
 
+
 StringReverse(['h','e','l','l','o'])
+
+
+// Move Zeros
+
+function MoveZeros(arr){
+    let x = 0;
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] !== 0) {
+            arr[x] = arr[i];
+            x = x + 1;
+        }
+    }
+    for(let i = x; i<arr.length;i++){
+        arr[i] = 0;
+    }
+
+    return arr;
+}
+
+console.log(MoveZeros([1,0,0,3,0,3]))
