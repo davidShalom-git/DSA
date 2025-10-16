216c63d02bfcbf7151b7efa9f451ef751997903b
davidshalom-git <davidshalom2609@gmail.com>
2025-10-16 16:04:14 +0530

    DSA

--- Searching.js
+++ Searching.js
@@ -55,3 +55,26 @@ function BubbleSort(arr){
 
 console.log(BubbleSort([5,1,4,2,3]));
 
+
+
+// Selection
+
+function SelectionSort(arr){
+    let n = arr.length;
+    for(let i = 0;i<n-1;i++){
+        let min = i
+        for(let j = i+1;j<n;j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        let temp = arr[min]
+        arr[min] = arr[i];
+        arr[i] = temp
+    }
+
+    return arr
+}
+
+console.log(SelectionSort([5,1,4,2,3]));
