846710f56b9e9f5cf3cd7ec67e0be00d6e7c95ff
davidshalom-git <davidshalom2609@gmail.com>
2025-10-20 21:40:53 +0530

    DSA

---
+++ Practice.js
@@ -0,0 +1,15 @@
+// Return the No of Negatives in an array;
+
+function Negative(arr){
+    let count = 0;
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] < 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(Negative([0,1,2,3,-1,-2,5,-3]));
+
+
