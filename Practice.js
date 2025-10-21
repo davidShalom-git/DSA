e7fd0668e23bbcdb25dcbdbbdfee2f774ae31d62
davidshalom-git <davidshalom2609@gmail.com>
2025-10-21 21:19:17 +0530

    DSA

--- Practice.js
+++ Practice.js
@@ -176,4 +176,22 @@ function MaxOne(arr){
 console.log(MaxOne([0,1,1,0,1,1,1]));
 
 
-//
+// Missing a Number
+
+function MissingNumber(arr){
+    let n = arr.length;
+    let totalSum = Math.floor(n*(n+1)/2);
+    let partialSum = 0;
+    for(let i = 0;i<n;i++){
+        partialSum = partialSum + arr[i];
+    }
+    return totalSum-partialSum;
+}
+console.log(MissingNumber([4,0,2,1,5]));
+
+
+console.log("Above are the Arrays Question...........")
+
+
+// Recursion
+
