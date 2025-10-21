23b4ee1c7db34fcfe28a58dbc6aa5fbf3194d859
davidshalom-git <davidshalom2609@gmail.com>
2025-10-21 11:16:12 +0530

    DSA

--- Practice.js
+++ Practice.js
@@ -15,3 +15,14 @@ console.log(Negative([0,1,2,3,-1,-2,5,-3]));
 
 // Return the Largest element in an array
 
+function Largest(arr){
+    let max = 0;
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(Largest([2,3,5,100,-1,0]));
