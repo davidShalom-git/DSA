7947ddd81bd0c8a00b9d967651cbe57a80ed1e62
davidshalom-git <davidshalom2609@gmail.com>
2025-10-21 11:32:03 +0530

    DSA

--- Practice.js
+++ Practice.js
@@ -22,7 +22,30 @@ function Largest(arr){
             max = arr[i];
         }
     }
-    return max;
+    return "The Max is : " +  max;
 }
 
 console.log(Largest([2,3,5,100,-1,0]));
+
+
+// return secondLargest element
+
+function SecondLargest(arr){
+    let max = 0;
+    let SecondMax = 0;
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] > max){
+            SecondMax = max;
+            max = arr[i];
+        }
+        else if(arr[i] > SecondMax){
+            SecondMax = arr[i];
+        }
+    }
+    return "The SecondMax is " + SecondMax;
+}
+
+console.log(SecondLargest([2,3,5,100,-1,0]));
+
+
+
