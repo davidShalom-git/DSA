aacfa23d8ab154bd5eedda50717ab65cb1c4b262
davidshalom-git <davidshalom2609@gmail.com>
2025-10-21 19:00:22 +0530

    DSA

--- Practice.js
+++ Practice.js
@@ -134,3 +134,22 @@ function ReverseString(arr){
 }
 
 console.log(ReverseString(['h','e','l','l','0']));
+
+
+// Move Consecutive Zero
+
+function Zeros(arr){
+    let x = 0;
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i] !== 0){
+            arr[x] = arr[i]
+            x++;
+        }
+    }
+    for(let i = x;i<arr.length;i++){
+        arr[i] = 0;
+    }
+    return arr;
+}
+
+console.log(Zeros([0,1,0,3,2]));
