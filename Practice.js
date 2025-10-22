04f28779d7e65e2c9808a7247ed1a8cb44e542aa
davidshalom-git <davidshalom2609@gmail.com>
2025-10-22 20:04:01 +0530

    DSA

--- Practice.js
+++ Practice.js
@@ -302,3 +302,42 @@ console.log(BinarySearch([1,2,3,4,5],4))
 
 
 // Bubble Sort
+
+function BubbleSort(arr){
+
+    let n = arr.length
+    for(let i = 0;i<n-1;i++){
+        for(let j = 0;j<n-1-i;j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(BubbleSort([5,1,6,2]))
+
+
+// Selection Sort
+
+function SelectionSort(arr){
+    let n = arr.length;
+    for(let i = 0;i<n-1;i++){
+        let min = i;
+        for(let j = i+1;j<n;j++){
+            if(arr[j]<arr[min]){
+                min = j;
+
+            }
+        }
+        let temp = arr[i]
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+
+console.log(SelectionSort([5,1,6,2,7]))
