49446ef04816bbad519d91f93c4be1d0b9831e91
davidshalom-git <davidshalom2609@gmail.com>
2025-12-03 11:43:52 +0530

    DSA

--- Strings.js
+++ Strings.js
@@ -1,1 +1,49 @@
 // Length of the last word
+
+
+// reverse a vowel
+
+function reverseVowels(s){
+    let arr = s.split('');
+    let right = arr.length-1;
+    let left = 0
+    const vowels = 'aeiouAEIOU'
+
+    while(left<=right){
+        if(!vowels.includes(arr[left])){
+            left++;
+        }
+        else if(!vowels.includes(arr[right])){
+            right--;
+        }
+        else{
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+    return -1;
+}
+
+console.log(reverseVowels('hello'))
+
+
+function lastwordlength(s){
+    let n = s.length-1;
+    let count = 0;
+    while(n>=0){
+        if(s[n] !== ' '){
+            count++;
+        }
+        else if(count > 0){
+            break;
+        }
+        n--;
+    }
+    return count;
+}
+
+console.log(lastwordlength('  hello world  '));
